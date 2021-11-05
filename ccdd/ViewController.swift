6077f54eb9fd70be7af257fc3dc17e6987a13ee9
Takhir <60847855+TakhirFox@users.noreply.github.com>
2021-11-05 21:43:22 +0700

    Создан проект

---
+++ ccdd/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ccdd
+//
+//  Created by Zakirov Tahir on 05.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let hello = "Hello world"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+
+}
+
