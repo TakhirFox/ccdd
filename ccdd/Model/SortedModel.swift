696e8afd20a94354be4fb987d0f86c30d4b105ca
Takhir <60847855+TakhirFox@users.noreply.github.com>
2021-11-13 12:54:35 +0700

    Переместил файлы по MVC

---
+++ ccdd/Model/SortedModel.swift
@@ -0,0 +1,13 @@
+//
+//  SortedModel.swift
+//  ccdd
+//
+//  Created by Zakirov Tahir on 10.11.2021.
+//
+
+import Foundation
+
+struct SortedModel {
+    var name: String
+    var isSelected: Bool
+}
